bd49b2c1783c69010c4faec08caa011caa30924a
Yeo <guswls9281@gmail.com>
2022-06-17 17:57:53 +0900

    .

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 from resources.recipe import RecipeListResource
+from resources.recipe_info import RecipeResource
+
 
 
 app = Flask(__name__)
@@ -11,6 +13,7 @@ api = Api(app)
 
 # 경로와 리소스(API 코드)를
 api.add_resource(RecipeListResource,'/recipes')
+api.add_resource(RecipeResource,'/recipes/<int:recipe_id>') # 이 뒤에있는 숫자를 받아, 어디로 처리하라. 
 
 if __name__ == '__main__':
     app.run()
--- resources/recipe.py
+++ resources/recipe.py
@@ -1,6 +1,10 @@
+from http import HTTPStatus
+from multiprocessing import connection
 from flask import request
 from flask_restful import Resource
 from mysql.connector.errors import Error
+from mysql_connection import get_connection
+import mysql.connector
 from mysql_connection import get_connection
 
 ### API를 만들기 ㅜ이한 클래스 작성
@@ -22,6 +26,91 @@ class RecipeListResource(Resource):
         # 받아오는 코드
         data = request.get_json()
 
-        print(data)
+        # 받아온 데이터를 디비 저장하면 된다.
+        try:
+            # 데이터 insert 
+            # 1. DB에 연결
+            connection = get_connection()
 
-        return 
+            # 2. 쿼리문 만들기
+            query = '''insert into recipe
+                    (name, description, cook_time, directions)
+                    values
+                    (%s,%s,%s,%s);'''
+            record = (data['name'],data['description'], data['cook_time'],data['directions'] )
+
+            # 3. 커서를 가져온다.
+            cursor = connection.cursor()
+
+            # 4. 쿼리문을 커서를 이용해서 실행한다.
+            cursor.execute(query,record)
+
+            # 5. 커넥션을 커밋해줘야 한다 => 디비에 영구적으로 반영하라는 뜻
+            connection.commit()
+
+            # 6. 자원 해제
+            cursor.close()
+            connection.close()
+
+        except Error as e:
+            print(e)
+            cursor.close()
+            connection.close()
+            return {"error": str(e)}, 503
+
+        return {"result":"success"},200
+
+
+    def get(self):
+        # 저장되어있는 모든 API를 가져오는 것. 우선 mysql로 간다. 쿼리문을 가져와야하기 때문
+        # 쿼리 스트링으로 오는 데이터는 아래처럼 처리해준다.
+        offset = request.args.get('offset')
+        limit = request.args.get('limit')
+
+        # 디비로부터 데이터를 받아서, 클라이언트에 보내준다.
+        try :
+            connection = get_connection()
+
+            query = '''select *
+                    from recipe
+                    limit '''+offset+''' , '''+limit+''';'''
+
+
+        # select문은, dictionary = True를 해준다.
+            cursor = connection.cursor(dictionary = True)
+
+            cursor.execute(query)
+
+            # select문은, 아래 함수를 이용해서, 데이터를 가져온다. 
+            result_list = cursor.fetchall()
+            
+            print(result_list)
+
+            # 중요!
+            # 디비에서 가져온 timestamp는 파이썬의 datetime으로 자동 변경 된다. 
+            # 문제는! 이 데이터를 json으로 바로 보낼수 없으므로, 문자열로 바꿔서 다시 저장해서 보낸다. 
+
+            i = 0
+            for record in result_list :
+                # 한 행씩 가져와서, 그 행에 들어있는 i번째의 created_at을 ios 포맷으로 바꿔라.  
+                result_list[i]['created_at'] = record['created_at'].isoformat()
+                result_list[i]['updated_at'] = record['updated_at'].isoformat()
+                i = i + 1             
+                # 
+            cursor.close()
+            connection.close()
+
+
+        except mysql.connector.Error as e :
+            print(e)
+            cursor.close()
+            connection.close()
+
+            return {"error" : str(e)}, 503
+            # 503으로 보내겠다.
+        
+        return { "result" : "success" , 
+                "count" : len(result_list) ,
+                "result_list" : result_list }, 200
+                # 첫번째 키값
+        
